3c31c029e426be1514adb5894b155d5bbc35a1a7
Sergio Ojeda <soj3da@gmail.com>
2023-06-19 15:49:16 -0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+
+export default defineConfig({
+  plugins: [
+    laravel({ input: ["resources/css/app.css", "resources/js/app.tsx"], refresh: true }),
+    react(),
+    checker({ typescript: true }),
+    {
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith(".blade.php")) {
+          server.ws.send({ path: "*", type: "full-reload" });
+        }
+      },
+      name: "blade",
+    },
+  ],
+});
